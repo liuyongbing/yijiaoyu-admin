ef0d1e7ee639e153ea24afe76889666d6cb1595f
liuyongbing <mike.doon.lau@gmail.com>
2018-05-21 17:07:26 +0800

    Header: nav

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -15,19 +15,19 @@
                         <li>
                             <a href="{{ route('trainers.index') }}">{{ trans('models.trainers') }}管理</a>
                         </li-->
-                        <li class="{{ in_array(Route::currentRouteName(), ['grades.index', 'grades.edit']) ? 'active' : '' }}">
+                        <li class="{{ in_array(Route::currentRouteName(), ['grades.index', 'grades.edit', 'grades.add']) ? 'active' : '' }}">
                             <a href="{{ route('grades.index') }}">{{ trans('models.grades') }}管理</a>
                         </li>
-                        <li class="{{ in_array(Route::currentRouteName(), ['courses.index', 'courses.edit']) ? 'active' : '' }}">
+                        <li class="{{ in_array(Route::currentRouteName(), ['courses.index', 'courses.edit', 'courses.add']) ? 'active' : '' }}">
                             <a href="{{ route('courses.index') }}">{{ trans('models.courses') }}管理</a>
                         </li>
-                        <li class="{{ in_array(Route::currentRouteName(), ['teachings.index', 'teachings.edit']) ? 'active' : '' }}">
+                        <li class="{{ in_array(Route::currentRouteName(), ['teachings.index', 'teachings.edit', 'teachings.add']) ? 'active' : '' }}">
                             <a href="{{ route('teachings.index') }}">{{ trans('models.teachings') }}管理</a>
                         </li>
-                        <li class="{{ in_array(Route::currentRouteName(), ['news.index', 'news.edit']) ? 'active' : '' }}">
+                        <li class="{{ in_array(Route::currentRouteName(), ['news.index', 'news.edit', 'news.add']) ? 'active' : '' }}">
                             <a href="{{ route('news.index') }}">{{ trans('models.news') }}管理</a>
                         </li>
-                        <li class="{{ in_array(Route::currentRouteName(), ['accounts.index', 'accounts.edit']) ? 'active' : '' }}">
+                        <li class="{{ in_array(Route::currentRouteName(), ['accounts.index', 'accounts.edit', 'accounts.add']) ? 'active' : '' }}">
                             <a href="{{ route('accounts.index') }}">{{ trans('models.accounts') }}管理</a>
                         </li>
                     </ul>
