dd6487d971a1d86e54d09b368233ba17dfae6a34
liuyongbing <mike.doon.lau@gmail.com>
2018-05-23 21:22:34 +0800

    Login form

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -10,7 +10,7 @@
             </div>
             <div class="row">
                 <div class="col-lg-12">
-                    <form method="post" action="{{ route('auth') }}" class="bootstrap-admin-login-form">
+                    <form id="form_login" method="post" action="{{ route('auth') }}" class="bootstrap-admin-login-form">
                         {{ csrf_field() }}
                         <!--div style="height:38px;width:100%;float:left;margin-left:-20px;margin-top:-20px;margin-bottom:15px;">
                             <img src="{{ asset(elixir('images/logo-ocl.png')) }}" style="margin:auto 0;">
@@ -20,10 +20,11 @@
                             <input id="mobile" class="form-control" type="text" name="Login[mobile]" placeholder="手机号">
                         </div>
                         <div class="form-group" style="overflow:hidden;">
-                            <input class="form-control code-input" type="text" name="Login[code]" placeholder="验证码">
-                            <a id="get_code" href="javascript:void(0);" class="btn btn-lg btn-default code-btn">获取验证码</a>
+                            <input id="code" class="form-control code-input" type="text" name="Login[code]" placeholder="验证码">
+                            <input id="get_code" type="button" class="btn btn-lg btn-default code-btn" value="获取验证码" onclick="sendcode()" /> 
+
                         </div>
-                        <button class="btn btn-lg btn-primary" type="submit">登录</button>
+                        <button id="btn_login" class="btn btn-lg btn-primary" type="submit">登录</button>
                     </form>
                 </div>
             </div>
@@ -34,25 +35,59 @@
 @section('script')
 <script type="text/javascript">
     $(document).ready(function() {
-        $('#get_code').click(function() {
+        $('#btn_login').click(function() {
             var mobile = $('#mobile').val();
             if (!mobile) {
-                alert('请输入登录手机号');
+                alert('请输入手机号');
                 return false;
             }
-            $.ajax({
-                method: "POST",
-                url: "{{ route('sms.send') }}",
-                data: { 
-                    _token : '{{ csrf_token() }}',
-                    mobile : mobile 
-                },
-                dataType: 'json',
-                success: function(response) {
-                    
-                }
-            });
+            var code = $('#code').val();
+            if (!code) {
+                alert('请输入验证码');
+                return false;
+            }
+            $('#form_login').submit();
         });
     });
+    //验证码
+    var countdown=60; 
+    function sendcode() {
+        var mobile = $('#mobile').val();
+        if (!mobile) {
+            alert('请输入手机号');
+            return false;
+        }
+        
+        $.ajax({
+            method: "POST",
+            url: "{{ route('sms.send') }}",
+            data: { 
+                _token : '{{ csrf_token() }}',
+                mobile : mobile 
+            },
+            dataType: 'json',
+            success: function(response) {
+                
+            }
+        });
+        var obj = $("#get_code");
+        settime(obj);
+    }
+    function settime(obj) { //发送验证码倒计时
+        if (countdown == 0) { 
+            obj.attr('disabled',false); 
+            //obj.removeattr("disabled"); 
+            obj.val("获取验证码");
+            countdown = 60; 
+            return;
+        } else { 
+            obj.attr('disabled',true);
+            obj.val("重新发送(" + countdown + ")");
+            countdown--; 
+        } 
+        setTimeout(function() { 
+            settime(obj) 
+        } ,1000) 
+    }
 </script>
 @endsection
