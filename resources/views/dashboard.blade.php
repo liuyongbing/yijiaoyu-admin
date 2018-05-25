e0b725ed433f4d32ca694aba323bd632fe70b996
liuyongbing <mike.doon.lau@gmail.com>
2018-05-25 11:53:19 +0800

    List

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,32 +1,22 @@
-@extends('layouts.base')
+@extends('layouts.main')
 
 @section('title', trans('common.app_name'))
 
-@section('body')
-    <body class="bootstrap-admin-with-small-navbar">
-        @include('layouts.header')
-        @include('layouts.nav', ['verticalNavbarActive'=>''])
-
-        <div class="container">
-            <div class="row">
-                <div class="col-md-12">
-                    <div class="row">
-                        <div class="col-lg-12">
-                            <div class="page-header">
-                                <div style="font-size: 20px;">欢迎登录后台，请选择菜单进行管理操作</div>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div class="row">
-                        <div class="col-lg-12">
-                        </div>
-                    </div>
-
+@section('content')
+    <div class="col-md-12">
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="page-header">
+                    <div style="font-size: 20px;">欢迎登录后台，请选择菜单进行管理操作</div>
                 </div>
             </div>
         </div>
-        @include('layouts.footer')
-    </body>
+
+        <div class="row">
+            <div class="col-lg-12">
+            </div>
+        </div>
+
+    </div>
 @endsection
 
