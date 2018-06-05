9dad0003b45669ee1520354979b2925e807e2dbd
liuyongbing <mike.doon.lau@gmail.com>
2018-06-05 15:38:06 +0800

    Members

--- resources/views/form/team.blade.php
+++ resources/views/form/team.blade.php
@@ -3,7 +3,7 @@
     <select name="Record[team_type]" class="form-control" >
         <option value="">请选择</option>
         @foreach($teams as $key => $team)
-        <option value="{{ $key }}" @if(!empty($item['team_id']) && $key == $item['team_id'])) selected="selected" @endif>{{ $team }}</option>
+        <option value="{{ $key }}" @if(!empty($item['team_type']) && $key == $item['team_type'])) selected="selected" @endif>{{ $team }}</option>
         @endforeach
     </select>
 </div>
