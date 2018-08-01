88ba04a0fcf071f65a84726a24dc6cb1ca58da86
liuyongbing <mike.doon.lau@gmail.com>
2018-08-01 09:21:18 +0800

    develop_teacher_login

--- app/Constants/Dictionary.php
+++ app/Constants/Dictionary.php
@@ -17,8 +17,13 @@ class Dictionary
     const PAGE_SIZE = 10;//页容量
     
     const ACCOUNT_TYPE = [
-        'ADMINISTRATOR' => 1,//超级级管理员
-        'EDITOR'        => 2,//普通管理员
-        'TEACHER'       => 3,//教练
+        'ADMINISTRATOR'         => 1,//超级级管理员
+        'EDITOR'                => 2,//普通管理员
+        //'TEACHER'               => 3,//教练
+        'TEACHER_TAEKWONDO'     => 31,//教练:齐天大圣(跆拳道)
+        'TEACHER_POCKETCAT'     => 32,//教练:口袋猫(舞蹈)
+        'TEACHER_TOWN'          => 33,//教练:童画镇(绘画)
+        'TEACHER_SKATING'       => 34,//教练:学会玩(轮滑)
+        'TEACHER_BASKETBALL'    => 35,//教练:晓虎队(篮球)
     ];
 }
