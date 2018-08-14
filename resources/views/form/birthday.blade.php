60e2ac4f10cf9a38155d9e25982ebd5eb5c1cc78
liuyongbing <mike.doon.lau@gmail.com>
2018-08-14 10:17:57 +0800

    develop_student: datepicker

--- resources/views/form/birthday.blade.php
+++ resources/views/form/birthday.blade.php
@@ -1,4 +1,4 @@
 <label class="col-md-2 control-label">{{ trans('inputs.birthday') }}<span class="star-must">*</span></label>
 <div class="col-md-10">
-    <input name="Record[birthday]" class="form-control" id="birthday" type="text" value="@if(null !== old('Record')['birthday']){{ old('Record')['birthday'] }}@else{{ isset($item['birthday']) ? $item['birthday'] : '' }}@endif">
+    <input name="Record[birthday]" class="form-control datepicker" id="birthday" type="text" value="@if(null !== old('Record')['birthday']){{ old('Record')['birthday'] }}@else{{ isset($item['birthday']) ? $item['birthday'] : '' }}@endif">
 </div>
